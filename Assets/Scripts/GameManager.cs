ffb23c41993b2ad85a89789b435716ddbf5531ff
RyotaTsushima <ryota.00.08.08@gmail.com>
2024-08-03 16:19:57 +0900

    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject[] _mouse;
     public List<GameObject> _cat = new List<GameObject>();
     [SerializeField] UnityEvent _startEvent;
-
+    int _target;
     bool _commanadPhase;
+    int _mouseSelect;
+    bool _select;
+    int _comSelect;
     bool _battlePhase;
     
     void Start()
@@ -20,6 +23,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_commanadPhase)
+        {
+            if (_select == false)
+            {
+                if(Input.GetKey(KeyCode.Return)) 
+                { 
+                    _select = true;
+                }
+            }
+            else
+            {
+                _comSelect = 0;
+                if (Input.GetKeyDown(KeyCode.S) && _comSelect>0)
+                {
+                    _comSelect++;
+                }
+
+                if (Input.GetKeyDown(KeyCode.W) && _comSelect<2) 
+                {
+                    _comSelect--;
+                }
+            }
+        }
     }
 }
